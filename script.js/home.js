2b48ffd0ec47ff41e63c73a97dde52bb13a59789
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-03-07 04:26:38 +0600

    all btn labels done

--- script.js/home.js
+++ script.js/home.js
@@ -1,16 +1,36 @@
 // console.log("Home connected")
 const cardContainer = document.getElementById('all-cards-container');
+const loadingSpinner = document.getElementById('loading-spinner');
+let count = 0;
+
+const createElement = (labels) =>{
+   let label = labels.map(item => `<span class="text-[12px] bg-amber-300 p-1 rounded-xl">${item}</span>`)
+   return (label.join(" "));
+}
+// Loading Spinner
+const showSpinner = () => {
+    loadingSpinner.classList.remove('hidden')
+    cardContainer.classList.add('hidden')
+}
+const hideSpinner = () => {
+    loadingSpinner.classList.add('hidden')
+    cardContainer.classList.remove('hidden')
+}
 
 const loadAllIssues = async () => {
+    showSpinner()
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
     const res = await fetch(url);
     const data = await res.json();
     displayAllIssues(data.data)
+    hideSpinner()
+
 }
 
 const displayAllIssues = (cardsData) => {
     cardContainer.innerHTML = "";
     cardsData.forEach(element => {
+        count++;
         // console.log(element)
         const card = document.createElement('div');
         card.className = `card bg-base-100 shadow-2xl h-64 border-t-4 ${element.status == 'open' ? "border-t-green-700" : "border-t-red-700"} p-4 `;
@@ -20,8 +40,8 @@ const displayAllIssues = (cardsData) => {
                     <h2 class="text-[14px] font-semibold mb-3.5">${element.title}</h2>
                     <p class="text-[12px] text-[#64748B] line-clamp-2">${element.description}</p>
                     <div class="flex gap-1.5">
-                        <div class="badge badge-soft badge-error max-w-[50%]"><i class="fa-solid fa-bug"></i> <span class="text-[12px]">BUG</span></div>
-                        <div class="badge badge-soft badge-warning mb-3.5"><i class="fa-regular fa-life-ring"></i>  <span class="text-[12px]">HELP WANTED</span></div>
+                        <div class="">${createElement(element.labels)}</div>
+                        
                         
                     </div>
                     <p  class="text-[12px] text-[#64748B] mb-3.5">${element.author}</p>
