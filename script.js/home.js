17e12e082cd0507f86d6f28048a18fdf491225d1
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-03-07 03:45:39 +0600

    all filter button

---
+++ script.js/home.js
@@ -0,0 +1,35 @@
+// console.log("Home connected")
+const cardContainer = document.getElementById('all-cards-container');
+
+const loadAllIssues = async () => {
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAllIssues(data.data)
+}
+
+const displayAllIssues = (cardsData) => {
+    cardContainer.innerHTML = "";
+    cardsData.forEach(element => {
+        // console.log(element)
+        const card = document.createElement('div');
+        card.className = `card bg-base-100 shadow-2xl h-64 border-t-4 ${element.status == 'open' ? "border-t-green-700" : "border-t-red-700"} p-4 `;
+        card.innerHTML = `
+        <div>
+                    <div class="flex justify-end mb-3.5"><div class="badge badge-soft badge-error">${element.priority}</div></div>
+                    <h2 class="text-[14px] font-semibold mb-3.5">${element.title}</h2>
+                    <p class="text-[12px] text-[#64748B] line-clamp-2">${element.description}</p>
+                    <div class="flex gap-1.5">
+                        <div class="badge badge-soft badge-error max-w-[50%]"><i class="fa-solid fa-bug"></i> <span class="text-[12px]">BUG</span></div>
+                        <div class="badge badge-soft badge-warning mb-3.5"><i class="fa-regular fa-life-ring"></i>  <span class="text-[12px]">HELP WANTED</span></div>
+                        
+                    </div>
+                    <p  class="text-[12px] text-[#64748B] mb-3.5">${element.author}</p>
+                    <p  class="text-[12px] text-[#64748B] ">${element.createdAt}</p>
+                </div> 
+        
+        `;
+        cardContainer.appendChild(card)
+
+    });
+}
