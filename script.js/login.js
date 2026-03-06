ebbe22330efe83cacdf5b7ef2749d91efa097ff0
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-03-07 00:41:16 +0600

    login section done

--- script.js/login.js
+++ script.js/login.js
@@ -1,5 +1,18 @@
 // console.log("Connected");
-document.getElementById('id').addEventListener('click', ()=>{
-    window.location.assign("index.home.html")
+document.getElementById('login-btn').addEventListener('click', ()=>{
+    const usernameInput = document.getElementById('username-input');
+    const username =usernameInput.value;
+    const passwordInput = document.getElementById('password-input');
+    const password = passwordInput.value;
+    
+    if(username === "admin" && password === "admin123"){
+        
+        window.location.assign("index.home.html")
+    }
+    else{
+        alert("Invalid username or password");
+        return;
+    }
+    // console.log(username, password)
 })
 
