d2b6f457a11ccc2a6912ef1b2dc41adc53d315f6
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-03-08 01:00:08 +0600

    modal fixed

--- script.js/home.js
+++ script.js/home.js
@@ -4,6 +4,7 @@ const loadingSpinner = document.getElementById('loading-spinner');
 const allFilterBtn = document.getElementById('all-filter-btn')
 const openFilterBtn = document.getElementById('open-filter-btn')
 const closedFilterBtn = document.getElementById('closed-filter-btn');
+const modalContainer = document.getElementById('modal-container');
 let totalIssues = document.getElementById('total-issue');
 
 let count = 0;
@@ -20,6 +21,48 @@ const showSpinner = () => {
 const hideSpinner = () => {
     loadingSpinner.classList.add('hidden')
     cardContainer.classList.remove('hidden')
+}
+
+const loadModal = async (id) => {
+    // console.log(id)
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await res.json();
+
+    displayModal(data.data)
+}
+
+const displayModal = (data) => {
+    const modal = document.getElementById('my_modal_1')
+    console.log(data)
+    modal.showModal()
+    const modalDiv = document.createElement('div');
+    modalContainer.innerHTML = "";
+    modalDiv.innerHTML = `
+        <h1 class="text-2xl font-bold mb-2.5">${data.title}</h1>
+                <div class="flex gap-2 items-center mb-6">
+                    <p class="${data.status == 'open' ? "bg-green-500":"bg-purple-500"} text-white text-[12px] py-1.5 px-2 rounded-2xl">${data.status == 'open'? "Opend" : "Closed"}</p>
+                    <p class="text-[#64748B] text-[12px]">Opened by <span>${data.author}</span></p>
+                    <p class="text-[#64748B] text-[12px]">${data.createdAt}</p>
+                </div>
+                <div class="flex gap-1.5 mb-6">
+                    <div class="">${createElement(data.labels)}</div>
+                </div>
+                <p class="text-[#64748B] mb-7">${data.description}</p>
+                <div class="flex gap-20">
+                    <div class="">
+                        <h1 class="text-[#64748B]">Assignee:</h1>
+                        <p class="font-bold">${data.assignee}</p>
+                    </div>
+                    <div class="">
+                        <h1 class="text-[#64748B]">Priority:</h1>
+                        <p class="font-bold text-center ${data.priority == 'high' ? "bg-red-500": data.priority == 'medium' ? "bg-amber-400" : "bg-gray-500"} text-white text-[12px] py-1 px-2 rounded-2xl">${data.priority.toUpperCase()}</p>
+                    </div>
+                </div>
+    
+    `;
+    modalContainer.appendChild(modalDiv)
+
+    // console.log(data);
 }
 
 const loadAllIssues = async () => {
@@ -40,15 +83,15 @@ const displayAllIssues = (cardsData) => {
     cardContainer.innerHTML = "";
     cardsData.forEach(element => {
         count++;
-        // console.log(element)
+        // console.log(element.id)
         const card = document.createElement('div');
         card.className = `card bg-base-100 shadow-2xl h-64 border-t-4 ${element.status == 'open' ? "border-t-green-700" : "border-t-purple-700"} p-4 `;
         card.innerHTML = `
-        <div>   
+        <div onclick="loadModal(${element.id})" class="cursor-pointer">   
                     
                     <div class="flex justify-between mb-3.5" >
                     <p>${element.status == 'open' ? `<img src="assets/Open-Status.png"></img>` : `<img src="assets/Closed- Status .png" alt="">`}</p>
-                    <div class="badge badge-soft badge-error">${element.priority}</div></div>
+                    <div class="badge badge-soft ${element.priority == 'high'?"badge-error": element.priority == 'medium'? "badge-warning" : "badge-neutral"}">${element.priority.toUpperCase()}</div></div>
                     <h2 class="text-[14px] font-semibold mb-3.5">${element.title}</h2>
                     <p class="text-[12px] text-[#64748B] line-clamp-2">${element.description}</p>
                     <div class="flex gap-1.5">
@@ -88,11 +131,11 @@ const displayOpenIssues = (cardsData) => {
             const card = document.createElement('div');
             card.className = `card bg-base-100 shadow-2xl h-64 border-t-4 ${element.status == 'open' ? "border-t-green-700" : "border-t-purple-700"} p-4 `;
             card.innerHTML = `
-        <div>   
+        <div onclick="loadModal(${element.id})">   
                     
                     <div class="flex justify-between mb-3.5">
                     <p>${element.status == 'open' ? `<img src="assets/Open-Status.png"></img>` : `<img src="assets/Closed- Status .png" alt="">`}</p>
-                    <div class="badge badge-soft badge-error">${element.priority}</div></div>
+                    <div class="badge badge-soft ${element.priority == 'high'?"badge-error": element.priority == 'medium'? "badge-warning" : "badge-neutral"}">${element.priority.toUpperCase()}</div></div>
                     <h2 class="text-[14px] font-semibold mb-3.5">${element.title}</h2>
                     <p class="text-[12px] text-[#64748B] line-clamp-2">${element.description}</p>
                     <div class="flex gap-1.5">
@@ -134,11 +177,11 @@ const displayClosedIssues = (cardsData) => {
             const card = document.createElement('div');
             card.className = `card bg-base-100 shadow-2xl h-64 border-t-4 ${element.status == 'open' ? "border-t-green-700" : "border-t-purple-700"} p-4 `;
             card.innerHTML = `
-        <div>   
+        <div onclick="loadModal(${element.id})">   
                     
                     <div class="flex justify-between mb-3.5">
                     <p>${element.status == 'open' ? `<img src="assets/Open-Status.png"></img>` : `<img src="assets/Closed- Status .png" alt="">`}</p>
-                    <div class="badge badge-soft badge-error">${element.priority}</div></div>
+                    <div class="badge badge-soft ${element.priority == 'high'?"badge-error": element.priority == 'medium'? "badge-warning" : "badge-neutral"}">${element.priority.toUpperCase()}</div></div>
                     <h2 class="text-[14px] font-semibold mb-3.5">${element.title}</h2>
                     <p class="text-[12px] text-[#64748B] line-clamp-2">${element.description}</p>
                     <div class="flex gap-1.5">
@@ -164,12 +207,12 @@ document.getElementById('search-btn').addEventListener('click', () => {
     const searchValue = input.value.trim().toLowerCase();
     // console.log(searchValue)
     fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
-    .then(res => res.json())
-    .then(data => {
-       const allCards = data.data;
-    //    console.log(allCards)
-    const filterCards = allCards.filter(item => item.title.toLowerCase().includes(searchValue))
-    displayAllIssues(filterCards)
-    })
+        .then(res => res.json())
+        .then(data => {
+            const allCards = data.data;
+            //    console.log(allCards)
+            const filterCards = allCards.filter(item => item.title.toLowerCase().includes(searchValue))
+            displayAllIssues(filterCards)
+        })
 })
 loadAllIssues()
